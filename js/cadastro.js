f046967300903f4ab4e349a6ca9c2f1ab30b9695
Marcelly Cirino <cellycirino@gmail.com>
2025-10-26 19:38:31 -0300

    validacao telefone cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -10,8 +10,19 @@
     }
 
     if (tipo === 'telefone') {
-      valor = valor.replace(/^(\d{2})(\d)/g, '($1) $2');
-      valor = valor.replace(/(\d{5})(\d)/, '$1-$2');
+      // Remove tudo que não for número
+      valor = valor.replace(/\D/g, '');
+
+      // Adiciona o formato (00) 00000-0000
+      if (valor.length > 10) {
+        valor = valor.replace(/^(\d{2})(\d{5})(\d{4}).*/, '($1) $2-$3');
+      } else if (valor.length > 5) {
+        valor = valor.replace(/^(\d{2})(\d{4,5})/, '($1) $2');
+      } else if (valor.length > 2) {
+        valor = valor.replace(/^(\d{2})/, '($1) ');
+      }
+
+      telefone.value = valor;
     }
 
     if (tipo === 'cep') {
